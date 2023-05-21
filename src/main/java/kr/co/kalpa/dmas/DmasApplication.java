696b0428382c7ef548eb14af1cf780449343875d
KimDoYoung <kdy987@gmail.com>
2023-05-21 17:33:20 +0900

    first commit

---
+++ src/main/java/kr/co/kalpa/dmas/DmasApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kalpa.dmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/kalpa/dmas/DmasApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kalpa.dmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
